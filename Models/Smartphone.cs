305adbc186705eb3976d1145bbdfe9ef778c9835
nathanlanna <nathan.lanna0801@gmail.com>
2024-01-08 08:10:38 -0300

    Desafio Smartphone POO

--- Models/Smartphone.cs
+++ Models/Smartphone.cs
@@ -3,12 +3,20 @@ namespace DesafioPOO.Models
     public abstract class Smartphone
     {
         public string Numero { get; set; }
-        // TODO: Implementar as propriedades faltantes de acordo com o diagrama
+        private string Modelo;
+        private string IMEI;
+        private int Memoria;
 
-        public Smartphone(string numero)
+
+        // Implementado
+
+        public Smartphone(string numero, string modelo, string imei, int memoria)
         {
             Numero = numero;
-            // TODO: Passar os parâmetros do construtor para as propriedades
+            this.Modelo = modelo;
+            this.IMEI = imei;
+            this.Memoria = memoria;
+            // Implementado
         }
 
         public void Ligar()
--- Program.cs
+++ Program.cs
@@ -1,3 +1,12 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+// Implementado
+Console.WriteLine("Nokia:");
+Nokia n1 = new Nokia(numero:"61911552233",modelo:"Tijolão 01",imei:"05010203096",memoria:32);
+n1.Ligar();
+n1.InstalarAplicativo("WhatsApp");
+
+Console.WriteLine("Iphone");
+Iphone i1 = new Iphone(numero:"61980808185", modelo:"ProMax 23",imei:"3333555666",memoria:128 );
+i1.ReceberLigacao();
+i1.InstalarAplicativo("Instagram");
